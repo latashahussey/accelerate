e436572ccd36463ef89714a42e54efc2ed9e1d04
sugarcookie <latasha.hussey@gmail.com>
2016-09-27 18:57:28 -0500

    single case study template

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -19,7 +19,6 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
-
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
