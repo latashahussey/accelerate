34a461b0907c8c9218330d8795efa57f4040df92
sugarcookie <latasha.hussey@gmail.com>
2016-10-01 13:11:57 -0500

    created advanced custom fields for case studies

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -16,8 +16,35 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post();
+
+				//store custom adv fields in php variable using built in function from ACF Plugin-->
+				$service = get_field('service'); //service provided to client
+				$client = get_field('client'); //client company name
+				$link = get_field('link');  //stores client url
+				$image1 = get_field('image1');
+				$image2 = get_field('image2');
+				$image3 = get_field('image3');
+			?>
+
+			<!--Display custom advanced fields on page-->
+			<aside class="case-study-sidebar">
+				<h2><?php  ?></h2>
+				<h5><?php $service ?></h5>
+				<h6>Client: <?php $client ?></h6>
+				<!--Now display the case study details-->
 				<?php the_content(); ?>
+				<a href="<?php $link ?>" target="_blank">Visit Live Site</a>
+			</aside>
+
+			<!--Display case study images--->
+			<div class="case-study-images">
+				<img class="alignnone" src="<?php $image1 ?>"  />
+				<img class="alignnone" src="<?php $image2 ?>"  />
+				<img class="alignnone" src="<?php $image3 ?>"  />
+			</div>
+
+
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
