1fe54812a2767bb8646bbbde9952d565a028fe56
sugarcookie <latasha.hussey@gmail.com>
2016-10-08 17:29:46 -0500

    finally got the about page to alternate imgs and align paragraphs correctly

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -18,9 +18,9 @@ get_header('about'); ?>
 		<div id="about-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<h2><?php the_title(); ?></h2>
-				<p>
+				<span class="about-intro">
 					<?php the_content(); ?>
-				</p>
+				</span>
 			<?php endwhile; // end of the loop. ?>
 
       <?php
@@ -35,26 +35,26 @@ get_header('about'); ?>
 			 <!--Display custom advanced fields on page-->
 			 <section class="services">
 
-           <!--Display service images--->
-           <div class ="service-description">
+          <div class="service-icon">
+            <!--Display service image--->
+            <?php if($icon) { ?>
+                <?php echo wp_get_attachment_image( $icon, $size ); ?>
+            <?php } ?>
+          </div>
 
-             <?php if($icon) { ?>
-               <?php echo wp_get_attachment_image( $icon, $size ); ?>
-           <?php } ?>
-          
 
-           <!--Now display the service details-->
-           <h4><?php the_title(); ?></h4>
-           <p>
+           <div class="service-description">
+             <!--Display service title first, otherwise, nth-child floats will not work properly-->
+            <span class="service-title"><?php the_title(); ?></span>
+             <!--Now display the service details-->
              <?php the_content(); ?>
-           </p>
-				 </div>
+           </div>
+
+
 			 </section>
 			<?php endwhile; // end of the loop. ?>
       <?php wp_reset_query(); ?>
-
-
-		</div><!-- #content -->
+    </div><!-- #content -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
